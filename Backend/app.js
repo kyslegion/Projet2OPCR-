c75f8ec56d0e39aafc0128c6e3584e6358f355f8
kyslegion <kyslegion@gmail.com>
2023-04-07 06:11:44 +0200

    Correction structure et css 3

---
+++ Backend/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const swaggerUi = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerDocs = yaml.load('swagger.yaml')
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(helmet({
+      crossOriginResourcePolicy: false,
+    }));
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.options('*', cors())
+const db = require("./models");
+const userRoutes = require('./routes/user.routes');
+const categoriesRoutes = require('./routes/categories.routes');
+const worksRoutes = require('./routes/works.routes');
+db.sequelize.sync().then(()=> console.log('db is ready'));
+app.use('/api/users', userRoutes);
+app.use('/api/categories', categoriesRoutes);
+app.use('/api/works', worksRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+module.exports = app;
